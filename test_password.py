2fe4a609fa2a19bb32ef5098553a99f9bc24a88a
kiprotichdominic <kiprotichdominic102@gmail.com>
2019-12-14 17:02:54 +0300

    add test for saving the user name

--- test_password.py
+++ test_password.py
@@ -8,7 +8,9 @@ class TestUser(unittest.TestCase):
     def test_init(self):
         self.assertEqual(self.new_user.user_name, "Kiprotich")
         self.assertEqual(self.new_user.user_password, "12345678")
-
+        
+    def test_save_user(self):
+        self.new_user.save_user()
 
 
     def tearDown(self):
