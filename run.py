aafb1ab39326bbefb07248220079de8189f0fb03
kiprotichdominic <kiprotichdominic102@gmail.com>
2019-12-14 08:19:59 +0300

    Add input to users name

--- run.py
+++ run.py
@@ -8,7 +8,21 @@ def create_user(user_name, user_password):
 
 def save_user(user):
     user.save_user()
+
+
 def delete_user(user):
     user.delete_user()
+
+
 def find_user(username):
     return User.find_by_username(username)
+
+
+def diplay_user():
+    return User.display_user()
+
+
+def main():
+    user_name = input()
+    
+    print("Hello {User_name}. Welcome to password locker. Create strong passwords.")
