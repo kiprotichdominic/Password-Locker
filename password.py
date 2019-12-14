8cf77bd034f789396c2cd270e47d57fc3d697e8b
kiprotichdominic <kiprotichdominic102@gmail.com>
2019-12-14 07:00:01 +0300

    add function for saving and deleting password

--- password.py
+++ password.py
@@ -2,11 +2,18 @@ import random
 
 
 class User:
+    # saves user generated password
+    user_pass = []
 
-    User_password = []
+    def __init__(self, user_name, user_password):
+        self.user_name = user_name
+        self.user_password = user_password
 
-    def __init__(self):
-        pass
+    def save_password(self):
+        User.user_pass.append(self)
+
+    def delete_password(self):
+        User.user_pass.remove(self)
 
 
 class Credentials:
