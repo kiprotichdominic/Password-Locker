259b9820fdd1c9ce4e4b294b8a50279e3e542f33
kiprotichdominic <kiprotichdominic102@gmail.com>
2019-12-14 11:29:12 +0300

    create an if statement for creating user

--- password.py
+++ password.py
@@ -15,6 +15,16 @@ class User:
     def delete_user(self):
         User.user_pass.remove(self)
 
+    @classmethod
+    def find_by_username(cls, username):
+        for user in cls.user_pass:
+            if user.user_name == username:
+                return True
+
+    @classmethod
+    def display_user(cls):
+        return cls.user_pass
+
 
 class Credentials:
     pass
--- run.py
+++ run.py
@@ -18,11 +18,31 @@ def find_user(username):
     return User.find_by_username(username)
 
 
-def diplay_user():
+def display_user():
     return User.display_user()
 
 
 def main():
     user_name = input()
-    
-    print("Hello {User_name}. Welcome to password locker. Create strong passwords.")
+
+    print(
+        "Hello {User_name}. Welcome to password locker. Create strong passwords.")
+    print("\n")
+
+    while True:
+        print(
+            """
+Use these short codes:
+cu - create user
+du - display user
+fu - find user
+del - delete user
+esc - exit the program
+            """
+
+        )
+        short_code = input().lower()
+
+        if short_code == "cu":
+            print("Create an new user")
+            print("-" * 10)
