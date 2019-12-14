c3ae4a38eb8168e7a2d08cbd2cb5eebf1787f3b5
kiprotichdominic <kiprotichdominic102@gmail.com>
2019-12-14 17:00:52 +0300

    create first test for user name and password

--- test_password.py
+++ test_password.py
@@ -5,6 +5,9 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         self.new_user = User("Kiprotich", "12345678")
 
+    def test_init(self):
+        self.assertEqual(self.new_user.user_name, "Kiprotich")
+        self.assertEqual(self.new_user.user_password, "12345678")
 
 
 
